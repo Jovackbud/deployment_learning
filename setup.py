b0e3fd8e7dda5094327cd81dec7ca4649a1d1c9a
Jovackbud <victorkola97@gmail.com>
2023-04-17 15:02:49 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='salespredad',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='User',
+    author_email='victorkola97@gmail.com',
+    description='predicts expected sales from ad expenses'
+)
